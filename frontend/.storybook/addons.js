b66042bf51fb09ae3f48c05739fe3751d35ef642
Tomek I <tomek.iwainski@gmail.com>
2019-11-12 15:26:29 +1100

    added storybook addons

--- frontend/.storybook/addons.js
+++ frontend/.storybook/addons.js
@@ -1,1 +1,5 @@
 import "@storybook/addon-actions/register";
+import "@storybook/addon-viewport/register";
+import "@storybook/addon-knobs/register";
+import "storybook-addon-styled-component-theme/dist/register";
+import "@storybook/addon-backgrounds/register";
