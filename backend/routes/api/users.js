ca3ad45f7d6e5241c453f2fddba6552958abe88f
Tomek I <tomek.iwainski@gmail.com>
2019-11-20 16:39:22 +1100

    Update users.js
    additional routes
    middleware

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,13 +1,11 @@
 var router = require("express").Router();
 var user_controller = require("../../controllers/userController");
-//var passport = require('passport');
-
+var auth = require("../../middlewares/auth");
 // url: api/user/*
 
 router.get("/", user_controller.index);
-
+router.get("/current", auth, user_controller.current);
 router.get("/:username", user_controller.findByUsername);
-
 router.post("/create", user_controller.create);
 router.put("/:id", user_controller.update);
 router.delete("/:id", user_controller.delete);
