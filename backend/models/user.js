c2229f26e4b452ccee59d3af11c48900834cf75c
Tomek I <tomek.iwainski@gmail.com>
2019-11-20 16:39:02 +1100

    addiotional fields
    pw encryption
    token generation
    validation

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,5 +1,8 @@
+const debug = require("debug")("model:user");
 var mongoose = require("mongoose");
-
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const bcrypt = require("bcrypt");
 /**
  * @typedef UserModel
  * @type {object}
@@ -12,13 +15,23 @@ var mongoose = require("mongoose");
  */
 const UserSchema = new mongoose.Schema(
   {
+    firstname: {
+      type: String,
+      match: [/^[a-zA-Z]+$/, "is invalid"]
+    },
+    lastname: {
+      type: String,
+      match: [/^[a-zA-Z]+$/, "is invalid"]
+    },
     username: {
       type: String,
       lowercase: true,
       unique: true,
       required: [true, "can't be blank"],
       match: [/^[a-zA-Z0-9]+$/, "is invalid"],
-      index: true
+      index: true,
+      minlength: 4,
+      maxlength: 15
     },
     email: {
       type: String,
@@ -26,11 +39,65 @@ const UserSchema = new mongoose.Schema(
       unique: true,
       required: [true, "can't be blank"],
       match: [/\S+@\S+\.\S+/, "is invalid"],
-      index: true
-    }
+      index: true,
+      minlength: 5,
+      maxlength: 100
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+      maxlength: 255
+    },
+    isAdmin: Boolean
   },
   { timestamps: true }
 );
+UserSchema.pre("save", async function(next) {
+  debug("pre save");
+  const user = this;
+  // only hash the password if it has been modified (or is new)
+  if (!user.isModified("password")) {
+    debug("password not modified");
+    return next();
+  }
+  debug("hashing password: ", user.password);
+  const hash = await bcrypt.hash(user.password, 10);
+  user.password = hash;
+  debug("updating password: ", hash);
+  next();
+});
+UserSchema.methods.comparePassword = function(candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
+
+//custom method to generate authToken
+UserSchema.methods.generateAuthToken = function() {
+  debug("signing token");
+  const algo = config.get("jwt.algorithm");
+  const expi = config.get("jwt.expiry");
+  debug("algorithm used: ", algo);
+  debug("expiry used: ", expi);
+
+  const d = new Date();
+
+  const calculatedExpiresIn =
+    d.getTime() + 60 * 60 * 1000 - (d.getTime() - d.getMilliseconds()) / 1000;
+
+  const token = jwt.sign(
+    { _id: this._id, isAdmin: this.isAdmin },
+    config.get("secret"),
+    {
+      algorithm: algo,
+      expiresIn: expi // seconds or a timespan
+    }
+  );
+  debug("Generated Token: ", token);
+  return token;
+};
 
 /*
 TODO: PASSWORD HASHING
--- backend/validators/userValidator.js
+++ backend/validators/userValidator.js
@@ -2,12 +2,26 @@ const Joi = require("Joi");
 
 function validateUser(user) {
   const schema = {
+    firstname: Joi.string()
+      .optional()
+      .regex(/^[a-zA-Z]+$/), //no invalid chars, alpha
+    lastname: Joi.string()
+      .optional()
+      .regex(/^[a-zA-Z]+$/), //no invalid chars, alpha
     username: Joi.string()
       .required()
-      .alphanum(),
+      .alphanum()
+      .min(4)
+      .max(15),
     email: Joi.string()
       .email()
       .required()
+      .min(5)
+      .max(100),
+    password: Joi.string()
+      .required()
+      .min(6)
+      .max(255)
   };
   return Joi.validate(user, schema);
 }
