b359a2292b1c7c91aa13462d4e61362a35ac9605
Tomek I <tomek.iwainski@gmail.com>
2019-11-27 10:17:19 +1100

    Update index.js
    refactored

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -6,24 +6,9 @@ const config = require("config");
 router.use("/api", require("./api"));
 router.use("/admin", require("./admin"));
 
-router.use("/auth", require("./api/auth.js"));
+router.use("/", require("./api/auth"));
 
 router.get("/", async (req, res) => {
-  const token =
-    req.headers["x-access-token"] ||
-    req.headers["authorization"] ||
-    req.cookies.token;
-  if (token) {
-    try {
-      //if can verify the token, set req.user and pass to next middleware
-
-      const decoded = jwt.verify(token, config.get("secret"));
-      req.user_id = decoded;
-    } catch (ex) {}
-  }
-  if (!req.user && req.user_id) {
-    req.user = await User.findById(req.user_id).select("-password");
-  }
   res.render("home", {
     user: req.user
   });
