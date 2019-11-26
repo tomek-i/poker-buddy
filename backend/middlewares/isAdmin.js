042a1a380e0b75cfbf481de8f234a90e1a8c945c
Tomek I <tomek.iwainski@gmail.com>
2019-11-27 10:17:01 +1100

    Update isAdmin.js
    added debug as well loading user from db

--- backend/middlewares/isAdmin.js
+++ backend/middlewares/isAdmin.js
@@ -1,6 +1,9 @@
-const debug = require("debug")("auth:admin");
+const debug = require("debug")("middleware:isAdmin");
+const User = require("../models/user");
 
 module.exports = function(req, res, next) {
+  if (req.user_id) req.user = User.findById(req.user).select("-password");
+
   if (!req.user) {
     debug("No user in request found");
     res.status(400).send("You need to login to access this area.");
